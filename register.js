a54e091e7ce530622107f0bff1d0559e158a647c
tbp-creative-studio <teamtbpessports@gmail.com>
2026-07-30 20:24:21 +0600

    Implement user registration with Firebase Auth

--- register.js
+++ register.js
@@ -1,1 +1,27 @@
+import { auth } from "./firebase.js";
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
+const registerBtn = document.getElementById("registerBtn");
+
+registerBtn.addEventListener("click", async () => {
+
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    if (!email || !password) {
+        alert("Please enter Email and Password.");
+        return;
+    }
+
+    try {
+        await createUserWithEmailAndPassword(auth, email, password);
+
+        alert("Account Created Successfully!");
+
+        window.location.href = "login.html";
+
+    } catch (error) {
+        alert(error.message);
+    }
+
+});
